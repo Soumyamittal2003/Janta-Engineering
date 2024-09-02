64b9b2fb0c996ca2e7aeb34f61d4c4100755e780
Mantu98 <mantu07941@gmail.com>
2024-09-02 09:41:05 +0530

    updating product page

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="bg-white shadow-lg rounded-md overflow-hidden flex flex-col transform transition-transform hover:scale-105">
+    <div className="bg-white shadow-lg rounded-md overflow-hidden flex flex-col transform transition-transform hover:scale-105 w-full max-w-xs">
       <Link
         to={{
           pathname: "/equipment-category",
@@ -17,12 +17,15 @@ const ProductCard = ({ product }) => {
             className="object-contain w-full h-48 md:h-64 lg:h-72"
           />
         </div>
-        <div className="md:w-3/10 p-4 flex flex-col justify-between">
-          <div className="text-lg font-semibold">{product.productName}</div>
-          <div className="text-md text-gray-600 font-semibold">
-            {product.category}
+        <div className="p-4 flex bg-gray-100 flex-col justify-between">
+          <div className="text-lg font-semibold truncate">
+            {product.productName}
           </div>
           <div className="text-gray-500">Code: {product.productCode}</div>
+          <div className="text-md text-gray-600 font-semibold truncate">
+            {product.category}
+          </div>
+          {/* <div className="text-gray-500">Code: {product.productCode}</div> */}
         </div>
       </Link>
     </div>
