efe58c27fb00a104dc816654007dcc3c40cc4ad1
Corazor Technology Private Limited <153815210+Corazortechnology@users.noreply.github.com>
2024-10-22 12:08:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   build: {
+//     chunkSizeWarningLimit: 1600,
+//     outDir: "dist",
+//   },
+//   plugins: [react()],
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Ensures that the application works with Vercel routing
   build: {
     chunkSizeWarningLimit: 1600,
     outDir: "dist",
