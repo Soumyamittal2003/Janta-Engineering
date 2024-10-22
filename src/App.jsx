50c20efbc2f00c1bf3240b3cc96e0f6ea6a5ef58
Corazor Technology Private Limited <153815210+Corazortechnology@users.noreply.github.com>
2024-10-22 14:46:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,61 @@
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Homepage from "./Pages/Homepage";
+// import ProductPage from "./Pages/ProductPage";
+// import EquipmentCategoryDetailPage from "./Components/Productdetailpage";
+// import Contact from "./Pages/Contact";
+// import NotFound from "./Pages/NotFound";
+// import AddAdmin from "./Pages/Addadmin";
+// import AddProductForm from "./Pages/AddProduct";
+// import WhoAreWe from "./AboutUs/WhoAreWe";
+// import SignIn from "./Pages/Loginpage";
+// import AdminProducts from "./Pages/AdminProducts";
+// import PrivateRoute from "./Auth/PrivateRoute";
+// import AdminDashboard from "./Pages/Admindashboard";
+// import ServiceAndCalibration from "./ServiceAndCalibration/ServiceAndCalibration";
+
+// function App() {
+//   return (
+//     <Router basename="/">
+//       <Routes>
+//         <Route path="/" element={<Homepage />} />
+//         <Route path="/product" element={<ProductPage />} />
+//         <Route path="/contact-us" element={<Contact />} />
+//         <Route path="/WhoAreWe" element={<WhoAreWe />} />
+//         <Route
+//           path="/equipment-category"
+//           element={<EquipmentCategoryDetailPage />}
+//         />
+//         <Route
+//           path="/ServiceAndCalibration"
+//           element={<ServiceAndCalibration />}
+//         />
+
+//         {/* Public admin login route */}
+//         <Route path="/admin" element={<SignIn />} />
+
+//         {/* Protected admin routes */}
+//         {/* <Route path="/admin" element={<PrivateRoute />}>
+//           <Route path="addEquipment" element={<AddProductForm />} />
+//           <Route path="adminProduct" element={<AdminProducts />} />
+//           <Route path="register" element={<AddAdmin />} />
+//         </Route> */}
+
+//         <Route path="/admin/dashboard" element={<PrivateRoute />}>
+//           <Route index element={<AdminDashboard />} />
+//           <Route path="addEquipment" element={<AddProductForm />} />
+//           <Route path="register" element={<AddAdmin />} />
+//           <Route path="adminProduct" element={<AdminProducts />} />
+//         </Route>
+
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import ProductPage from "./Pages/ProductPage";
@@ -21,25 +79,13 @@ function App() {
         <Route path="/product" element={<ProductPage />} />
         <Route path="/contact-us" element={<Contact />} />
         <Route path="/WhoAreWe" element={<WhoAreWe />} />
-        <Route
-          path="/equipment-category"
-          element={<EquipmentCategoryDetailPage />}
-        />
-        <Route
-          path="/ServiceAndCalibration"
-          element={<ServiceAndCalibration />}
-        />
+        <Route path="/equipment-category" element={<EquipmentCategoryDetailPage />} />
+        <Route path="/ServiceAndCalibration" element={<ServiceAndCalibration />} />
 
-        {/* Public admin login route */}
+        
         <Route path="/admin" element={<SignIn />} />
 
-        {/* Protected admin routes */}
-        {/* <Route path="/admin" element={<PrivateRoute />}>
-          <Route path="addEquipment" element={<AddProductForm />} />
-          <Route path="adminProduct" element={<AdminProducts />} />
-          <Route path="register" element={<AddAdmin />} />
-        </Route> */}
-
+        
         <Route path="/admin/dashboard" element={<PrivateRoute />}>
           <Route index element={<AdminDashboard />} />
           <Route path="addEquipment" element={<AddProductForm />} />
@@ -54,3 +100,4 @@ function App() {
 }
 
 export default App;
+
